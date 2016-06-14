4f43df5574c1dd2a75f30748588915008ac7710a
Michael Charland <mike_ch_1@hotmail.com>
2016-06-14 11:57:18 -0400

    rework loading properties to use files

--- src/main/java/ca/todoist/util/LoadProperties.java
+++ src/main/java/ca/todoist/util/LoadProperties.java
@@ -1,8 +1,8 @@
 package ca.todoist.util;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 public class LoadProperties {
@@ -25,15 +25,16 @@ public class LoadProperties {
 
 	public Properties load(String filename) throws IOException {
 		Properties prop = new Properties();
-		InputStream in = LoadProperties.class.getClass().getResourceAsStream(filename);
-		if (null == in) {
+		File base = new File(".");
+		String file = base.getCanonicalPath() + "/src/test/java/" + filename;
+		if (!new File(file).exists()) {
 			String r = "";
-			File file = new File(".");			
-			for(String f : file.list()) {
-				r+= f + " ";
+			for (String f : base.list()) {
+				r += f + " ";
 			}
-			throw new IllegalArgumentException(filename + " not found. Dir is " + file.getAbsolutePath() + " and files in dir are: " + r);
-		}
+			throw new IllegalArgumentException(filename + " not found. Dir is " + base.getAbsolutePath() + " and files in dir are: " + r);
+		}				
+		FileInputStream in = new FileInputStream(file);
 		prop.load(in);
 		in.close();
 		return prop;
